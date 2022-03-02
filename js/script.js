51d1d668c5def9a534ea43d8fbe6a2b00554c11d
reduan <reduan15-11413@diu.edu.bd>
2022-03-02 13:49:18 +0600

    update two

--- js/script.js
+++ js/script.js
@@ -6,10 +6,26 @@ const toggleSearchResult = (displayStyle) => {
   document.getElementById("search-mobile").style.display = displayStyle;
 };
 
+const toggleMobileDetail = (displayStyle) => {
+  document.getElementById("mobile-details").style.display = displayStyle;
+};
+
+const toggleError = (displayStyle) => {
+  document.getElementById("error-message").style.display = displayStyle;
+};
+
+const toggleShowBtn = (dataLength) => {
+  //console.log(dataLength);
+  if (dataLength > 20) {
+    document.getElementById("showAll").style.display = "block";
+  } else {
+    document.getElementById("showAll").style.display = "none";
+  }
+};
 
 const searchMobile = () => {
   const searchField = document.getElementById("search-field");
-  const searchText = searchField.value;
+  const searchText = searchField.value.toLowerCase();
 
   //display loader
   toggleSpinner("block");
@@ -17,51 +33,90 @@ const searchMobile = () => {
 
   //clear data
   searchField.value = "";
-  
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displaySearchResult(data.data));
+
+  //show error message
+  if (searchText == "") {
+    const errorResult = document.getElementById("error-message");
+    errorResult.innerHTML = `
+    <h1 class="text-danger">Write Something What brand mobile are you find</h1>
+    `;
+    errorResult.style.display = "block";
+    //display none mobile detail
+    toggleMobileDetail("none");
+  } else {
+    //get data
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displaySearchResult(data.data));
+    toggleError("none");
+  }
 };
 
 const displaySearchResult = (mobiles) => {
   const searchResult = document.getElementById("search-result");
+
+  //clear previous results
   searchResult.textContent = "";
-  mobiles.slice(0, 20).forEach((mobile) => {
-    const div = document.createElement("div");
-    div.classList.add("col-3","rounded");
-    div.innerHTML = `
-        <div class="card ">
-          <img src="${mobile.image}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h3 class="card-title">${mobile.phone_name}</h3>
-            <h5 class="card-text">Brand: ${mobile.brand}</h5>
-            <div class="d-grid">
-              <button class="btn btn-primary btn-lg btn-detail" onclick="loadeMobileDetail('${mobile.slug}')" type="button">Go Detail</button>
+
+  //display show btn
+  toggleShowBtn(mobiles.length);
+
+  //show mobile data
+  if (mobiles.length == 0) {
+    const errorResult = document.getElementById("error-message");
+    errorResult.innerHTML = `
+    <h1 class="text-danger">No Result found</h1>
+    `;
+    errorResult.style.display = "block";
+    toggleSearchResult("none");
+  } else {
+    mobiles.slice(0, 20).forEach((mobile) => {
+      const div = document.createElement("div");
+      div.classList.add(
+        "col-12",
+        "col-sm-6",
+        "col-md-4",
+        "col-lg-3",
+        "rounded"
+      );
+      div.innerHTML = `
+          <div class="card ">
+            <img src="${mobile.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h4 class="card-title">${mobile.phone_name}</h4>
+              <h5 class="card-text">Brand: ${mobile.brand}</h5>
+              <div class="d-grid">
+                <button class="btn btn-primary btn-lg btn-detail" onclick="loadeMobileDetail('${mobile.slug}')" type="button">Go Detail</button>
+              </div>
             </div>
-          </div>
-        </div> 
-        `;
-    searchResult.appendChild(div);
-  });
+          </div> 
+          `;
+      searchResult.appendChild(div);
+    });
+  }
   toggleSpinner("none");
   toggleSearchResult("block");
 };
 
+//load individual mobile detail
 const loadeMobileDetail = (id) => {
   console.log(id);
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   fetch(url)
-  .then(res => res.json())
-  .then(data => displayMobileDetail(data.data));
-}
+    .then((res) => res.json())
+    .then((data) => displayMobileDetail(data.data));
+};
 
+//show individual mobile detail
 const displayMobileDetail = (mobile) => {
   const mobileDetail = document.getElementById("mobile-details");
+  ////clear previous results
   mobileDetail.textContent = "";
+
   const div = document.createElement("div");
-  div.classList.add('card', 'mb-3');
-  div.style.maxWidth  = "95%";
+  div.classList.add("card", "mb-3");
+  div.style.maxWidth = "95%";
   div.innerHTML = `
       <div class="row g-0">
         <div class="col-md-4 mobile-img">
@@ -75,7 +130,11 @@ const displayMobileDetail = (mobile) => {
           <div class="card-body">
             <h2 class="card-title">${mobile.name}</h2>
             <p class="card-text">
-              <span>Exp:</span> ${mobile.releaseDate}
+              <span>Exp:</span> ${
+                mobile.releaseDate
+                  ? mobile.releaseDate
+                  : "Release Date not found"
+              }
             </p>
             
             <table class="table">
@@ -106,8 +165,8 @@ const displayMobileDetail = (mobile) => {
                   <tr>
                     <th scope="row">Sensors</th>
                     <td colspan="2">
-                     ${mobile.mainFeatures.sensors.map((sensor)=>{
-                        return`  ${sensor}  `
+                     ${mobile.mainFeatures.sensors.map((sensor) => {
+                       return `  ${sensor}  `;
                      })}
 
                     </td>
@@ -121,8 +180,9 @@ const displayMobileDetail = (mobile) => {
                 </tbody>
               </table>
             </table>
-            ${mobile.others?
-              `<table class="table">
+            ${
+              mobile.others
+                ? `<table class="table">
                 <thead>
                   <tr>
                     <th scope="col"><h4>Other Information</h4></th>
@@ -159,7 +219,9 @@ const displayMobileDetail = (mobile) => {
                     <td colspan="2">${mobile.others?.WLAN}</td>
                   </tr>
                 </tbody>
-              </table>`:''}
+              </table>`
+                : ""
+            }
           </div>
         </div>
     </div>`;
